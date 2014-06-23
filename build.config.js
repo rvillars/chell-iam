bf930b1da11d63e8f5273f51a65488971d8dfa22
rovi <roger.villars@bluewin.ch>
2014-06-23 23:26:59 +0200

    karma config

--- build.config.js
+++ build.config.js
@@ -3,21 +3,24 @@
  */
 module.exports = {
 
+    /**
+     * These are the dependencies needed for this module and their order
+     */
     module_dependencies: [
-        //AngularJS
+        // AngularJS
         'bower-components/angular/angular.js',
         'bower-components/angular-resource/angular-resource.js',
         'bower-components/angular-mocks/angular-mocks.js',
 
-        //AnglularUI
+        // AnglularUI
         'bower-components/angular-bootstrap/ui-bootstrap-tpls.js',
 
-        //Underscore
+        // Underscore
         'bower-components/underscore/underscore.js',
         'bower-components/angular-underscore-module/angular-underscore-module.js',
         'bower-components/angular-underscore/angular-underscore.js',
 
-        //Misc
+        // Misc
         'bower-components/angular-md5/angular-md5.js',
         'bower-components/angular-base64/angular-base64.js',
         'bower-components/ng-table/ng-table.js'
