e1a821f7a3f87ee3cce6a54c0f383ba2dbd21aa7
Roger Villars <roger.villars@bluewin.ch>
2014-12-16 14:01:07 +0000

    more tests

--- tests/chell-iam.tests.js
+++ tests/chell-iam.tests.js
@@ -1,15 +1,42 @@
 'use strict';
 
-describe('UserListController', function() {
-    var scope;//we'll use this scope in our tests
+describe('IamUser', function() {
 
-    //mock Application to allow us to inject our own dependencies
+    //mock Module to allow us to inject our own dependencies
     beforeEach(angular.mock.module('chell-iam'));
+    
     // tests start here
+    
+    it('should exist',inject(function(IamUser) {
+        expect(IamUser).toBeDefined();
+    }));
 
-    it('should fetch list of users', inject(function(IamUser) {
+    it('should query list of users', inject(function(IamUser) {
         IamUser.query().then(function(users) {
             expect(users.length).toBe(2);
         });
+    }));
+    
+    it('should get the correct user', inject(function(IamUser) {
+        var adminId = '2819c223-7f76-453a-919d-413861904646';
+        IamUser.get(adminId).then(function(user) {
+            expect(user.length).toBe(1);
+            expect(user.id).toBe(adminId);
+            expect(user.userName).toBe('chellAdmin')
+        });
+    }));
+});
+
+describe('UserProfileController', function() {
+
+    //mock Module to allow us to inject our own dependencies
+    beforeEach(angular.mock.module('chell-iam'));
+    
+    // tests start here
+    
+    it('should be creatable', inject(function($rootScope, $controller, IamUser) {
+        var scope = $rootScope.$new();
+        var userListCtrl = $controller("UserProfileController", {$scope: scope, IamUser: IamUser });
+        expect(userListCtrl).toBeDefined();
     }));
 });
